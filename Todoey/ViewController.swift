709c40574dbbe6100fad20ce323ce7e840934bcc
Matthew Lin Hau Hei <matthewlin@lins-imac.local>
2018-07-06 15:25:38 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Lin Hau Hei, Matthew on 6/7/2018.
+//  Copyright © 2018 Lin Hau Hei, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
